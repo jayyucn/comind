a8223f27c3e2ac9063816d8f8d6b1e02c11e2a40
jayyucn <nalanrola@gmail.com>
2026-05-30 17:01:19 +0800

    build(vite): add allowed hosts config for dev server
    configure allowed hosts to allow comind.jayyu.cn for cross-origin dev access

--- comind/vite.config.ts
+++ comind/vite.config.ts
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    // 放行你的域名
+    allowedHosts: ["comind.jayyu.cn"]
+    // 如需全部放行：allowedHosts: "all"
+  }
 })
